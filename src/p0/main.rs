d3fe0a0941b5d55b52ddad845a7f4697d037569e
masinc <masinc000@gmail.com>
2020-06-13 20:41:15 +0900

    delete unused code

--- src/p0/main.rs
+++ src/p0/main.rs
@@ -1,31 +1,5 @@
-use std::path::{PathBuf};
-use std::io::{self, Result};
-
-fn pwd() -> io::Result<PathBuf> {
-    let pwd = std::env::current_dir()?;
-    io::Result::Ok(pwd)
-}
-
-fn pwd_str<'a>() -> Result<String> {
-    let pwd = pwd()?;
-    match pwd.to_str() {
-        Some(s) => Result::Ok(String::from(s)),
-        None => Result::Err(io::Error::from(io::ErrorKind::Other))
-    }
-}
-
-fn main() -> Result<()> {
+fn main(){
     let s = "stressed";
     let s: String = s.chars().rev().collect();
     println!("{}", s);
-
-    let pwd = pwd_str()?;
-    println!("{}", pwd);
-
-    Result::Ok(())
 }
-//=("abc")
-//(= + "Hello" "" "Wold")
-//( echo "Hello World" )
-/*(=
-)*/
